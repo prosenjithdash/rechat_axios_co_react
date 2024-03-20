2a88f2f3faa2d682affa86d57dd0db2e74e7e519
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-20 17:10:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-4xl text-blue-700'>Axios , Rechat and Awesome Components</h1>
+    
+      
+    </>
+  )
+}
+
+export default App
